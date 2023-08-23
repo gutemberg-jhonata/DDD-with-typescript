f45ceea2700e43bd40a5499039befcdc7db40272
Jhonata <gutemberg.jhonata@gmail.com>
2023-08-23 02:47:13 -0300

    refactoring entity with ddd

--- src/entity/customer.ts
+++ src/entity/customer.ts
@@ -2,15 +2,25 @@ class Customer {
 
     _id: string
     _name: string
-    _address: string
+    _address: string = ""
+    _activate: boolean = true
 
-    constructor(id: string, name: string, address: string) {
+    constructor(id: string, name: string) {
         this._id = id
         this._name = name
-        this._address = address
+        this.validate()
     }
 
-    get id(): string {
+    validate() {
+        if (this._id.length === 0) {
+            throw new Error("Id is required")
+        }
+        if (this._name.length === 0) {
+            throw new Error("Name is required")
+        }
+    }
+
+    /*get id(): string {
         return this._id
     }
 
@@ -26,6 +36,21 @@ class Customer {
 
     set address(address: string) {
         this._address = address
+    }*/
+
+    changeName(name: string) {
+        this._name = name
+    }
+
+    activate() {
+        if (this._address.length === 0) {
+            throw new Error("Address is mandatory to activate a customer")
+        }
+        this._activate = true
+    }
+
+    deactivate() {
+        this._activate = false
     }
 
 }
