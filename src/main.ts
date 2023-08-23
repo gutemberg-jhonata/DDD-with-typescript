4422e6ce599bcee9fa68df5b7f8b1d3f2c26fcc6
Jhonata <gutemberg.jhonata@gmail.com>
2023-08-23 13:33:07 -0300

    config run tests

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,6 @@ const address = new Address("Rua dois", 2, "12345-678", "Montes Claros")
 customer.address = address
 customer.activate()
 
-const item1 = new OrderItem("1", "Item 1", 10)
-const item2 = new OrderItem("2", "Item 2", 15)
-const order = new Order("1", "123", [item1, item2])
+//const item1 = new OrderItem("1", "Item 1", 10)
+//const item2 = new OrderItem("2", "Item 2", 15)
+//const order = new Order("1", "123", [item1, item2])
